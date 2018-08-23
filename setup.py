95c3cad0468f3515ec05cee78e10b1e1ff4dcb42
Hugo Rivera <6373447+roguh@users.noreply.github.com>
2018-08-22 20:56:34 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='master_password',
+      version='0.1',
+      description=' An implementation of the Master Password algorithm v3 with a nice CLI',
+      url='http://github.com/roguh/master_password.py',
+      author='Hugo Rivera',
+      author_email='flyingcircus@example.com',
+      long_description=readme(),
+      classifiers=[
+          'Topic :: Security',
+          'Topic :: Security :: Cryptography',
+          'Topic :: Utilities',
+          'Environment :: Console',
+          'Operating System :: Unix',
+          'Operating System :: POSIX :: Linux',
+          'Topic :: Internet',
+      ],
+      # TODO license=None,
+      packages=['master_password'],
+      install_requires=[
+          'scrypt', 'pyperclip'
+      ],
+      scripts=['bin/mpw-cli.py'],
+      setup_requires=["pytest-runner"],
+      tests_require=["pytest"],
+      zip_safe=False)
+
